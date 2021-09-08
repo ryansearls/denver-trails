8ff6e8441c84049780217d0d1aaf04d74f977f14
Ryan Searls <Ryan.searls@gmail.com>
2021-09-08 16:42:32 -0600

    added index route

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,2 +1,9 @@
 class TrailsController < ApplicationController
+
+  def index
+    trail = Trail.all
+    render json: trail.as_json
+  end 
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+get "/trails", controller: "trails", action: "index"
+
 end
